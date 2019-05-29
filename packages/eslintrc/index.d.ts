73c217fd0d59a25c3d919186d86812e7cc683598
bluelovers <codelovers@users.sourceforge.net>
2019-05-29 15:19:07 +0800

    publish new version

--- packages/eslintrc/index.d.ts
+++ packages/eslintrc/index.d.ts
@@ -12,56 +12,89 @@ declare const eslintrcJson: {
         "project": string;
     };
     "rules": {
+        "arrow-parens": string;
         "eol-last": string[];
+        "eqeqeq": string[];
         "getter-return": (string | {
             "allowImplicit": boolean;
         })[];
+        "no-buffer-constructor": string;
+        "no-case-declarations": string;
+        "no-catch-shadow": string;
         "no-compare-neg-zero": string;
+        "no-div-regex": string;
         "no-dupe-args": string;
         "no-dupe-keys": string;
         "no-duplicate-case": string;
+        "no-else-return": string;
         "no-empty-character-class": string;
         "no-ex-assign": string;
-        "no-extra-semi": string;
+        "no-extend-native": string;
         "no-extra-bind": string;
         "no-extra-label": string;
-        "no-extend-native": string;
+        "no-extra-semi": string;
+        "no-floating-decimal": string;
+        "no-invalid-this": string;
         "no-irregular-whitespace": (string | {
             "skipComments": boolean;
             "skipStrings": boolean;
             "skipTemplates": boolean;
             "skipRegExps": boolean;
         })[];
-        "camelcase": string;
-        "semi": string;
-        "@typescript-eslint/semi": string[];
-        "symbol-description": string;
-        "require-yield": string;
-        "prefer-rest-params": string;
-        "object-shorthand": string;
+        "no-label-var": string;
+        "no-multi-spaces": string;
+        "no-multi-str": string;
+        "no-new": string;
+        "no-octal-escape": string;
+        "no-proto": string;
+        "no-redeclare": string;
+        "no-return-assign": string[];
+        "no-return-await": string;
+        "no-self-assign": (string | {
+            "props": boolean;
+        })[];
+        "no-self-compare": string;
+        "no-sequences": string;
+        "no-shadow": string;
+        "no-shadow-restricted-names": string;
+        "no-throw-literal": string;
         "no-unexpected-multiline": string;
-        "@typescript-eslint/camelcase": string;
-        "no-floating-decimal": string;
-        "no-var": string;
-        "@typescript-eslint/unbound-method": string;
+        "no-unmodified-loop-condition": string;
+        "no-unused-expressions": (string | {
+            "allowShortCircuit": boolean;
+            "allowTernary": boolean;
+        })[];
+        "no-unused-vars": string;
+        "no-useless-call": string;
+        "no-useless-escape": string;
         "no-useless-rename": (string | {
             "ignoreDestructuring": boolean;
         })[];
-        "@typescript-eslint/indent": string[];
-        "@typescript-eslint/restrict-plus-operands": string;
-        "@typescript-eslint/member-naming": (string | {
-            "private": string;
+        "no-useless-return": string;
+        "no-var": string;
+        "no-with": string;
+        "object-shorthand": string;
+        "prefer-promise-reject-errors": (string | {
+            "allowEmptyReject": boolean;
         })[];
-        "@typescript-eslint/require-array-sort-compare": string;
-        "@typescript-eslint/no-angle-bracket-type-assertion": string;
-        "@typescript-eslint/class-name-casing": string;
-        "@typescript-eslint/no-empty-interface": (string | {
-            "allowSingleExtends": boolean;
+        "prefer-rest-params": string;
+        "radix": string[];
+        "require-await": string;
+        "require-yield": string;
+        "symbol-description": string;
+        "vars-on-top": string;
+        "wrap-iife": (string | {
+            "functionPrototypeMethods": boolean;
         })[];
-        "@typescript-eslint/member-ordering": string;
-        "@typescript-eslint/prefer-includes": string;
         "@typescript-eslint/adjacent-overload-signatures": string;
-        "@typescript-eslint/no-useless-constructor": string;
+        "camelcase": string;
+        "@typescript-eslint/camelcase": string;
+        "@typescript-eslint/class-name-casing": string;
+        "@typescript-eslint/explicit-function-return-type": (string | {
+            "allowExpressions": boolean;
+        })[];
+        "@typescript-eslint/indent": string[];
+        "@typescript-eslint/interface-name-prefix": string[];
         "@typescript-eslint/member-delimiter-style": (string | {
             "multiline": {
                 "delimiter": string;
@@ -72,56 +105,24 @@ declare const eslintrcJson: {
                 "requireLast": boolean;
             };
         })[];
-        "@typescript-eslint/explicit-function-return-type": (string | {
-            "allowExpressions": boolean;
+        "@typescript-eslint/member-naming": (string | {
+            "private": string;
         })[];
-        "@typescript-eslint/interface-name-prefix": string[];
-        "@typescript-eslint/promise-function-async": (string | {
-            "allowedPromiseNames": string[];
+        "@typescript-eslint/member-ordering": string;
+        "@typescript-eslint/no-angle-bracket-type-assertion": string;
+        "@typescript-eslint/no-object-literal-type-assertion": string[];
+        "@typescript-eslint/no-empty-interface": (string | {
+            "allowSingleExtends": boolean;
         })[];
-        "no-unused-expressions": (string | {
-            "allowShortCircuit": boolean;
-            "allowTernary": boolean;
-        })[];
-        "no-throw-literal": string;
-        "no-case-declarations": string;
-        "no-sequences": string;
-        "no-unmodified-loop-condition": string;
-        "eqeqeq": string[];
-        "no-div-regex": string;
-        "no-multi-spaces": string;
-        "no-multi-str": string;
-        "no-new": string;
-        "no-proto": string;
-        "no-redeclare": string;
-        "no-octal-escape": string;
-        "no-else-return": string;
-        "no-return-assign": string[];
-        "no-return-await": string;
-        "no-self-compare": string;
-        "no-catch-shadow": string;
-        "no-label-var": string;
-        "no-buffer-constructor": string;
-        "no-shadow-restricted-names": string;
-        "no-useless-call": string;
-        "no-useless-return": string;
-        "no-unused-vars": string;
-        "arrow-parens": string;
-        "no-with": string;
-        "prefer-promise-reject-errors": (string | {
-            "allowEmptyReject": boolean;
-        })[];
-        "no-invalid-this": string;
-        "require-await": string;
-        "no-self-assign": (string | {
-            "props": boolean;
-        })[];
-        "vars-on-top": string;
-        "no-useless-escape": string;
-        "no-shadow": string;
-        "radix": string[];
-        "wrap-iife": (string | {
-            "functionPrototypeMethods": boolean;
+        "@typescript-eslint/explicit-member-accessibility": (string | {
+            "accessibility": string;
+            "overrides": {
+                "constructors": string;
+                "accessors": string;
+                "methods": string;
+                "properties": string;
+                "parameterProperties": string;
+            };
         })[];
         "@typescript-eslint/no-use-before-define": (string | {
             "functions": boolean;
@@ -129,7 +130,17 @@ declare const eslintrcJson: {
             "variables": boolean;
             "typedefs": boolean;
         })[];
+        "@typescript-eslint/no-useless-constructor": string;
+        "@typescript-eslint/prefer-includes": string;
         "@typescript-eslint/prefer-regexp-exec": string;
+        "@typescript-eslint/promise-function-async": (string | {
+            "allowedPromiseNames": string[];
+        })[];
+        "@typescript-eslint/require-array-sort-compare": string;
+        "@typescript-eslint/restrict-plus-operands": string;
+        "semi": string;
+        "@typescript-eslint/semi": string[];
+        "@typescript-eslint/unbound-method": string;
     };
 };
 export = eslintrcJson;
